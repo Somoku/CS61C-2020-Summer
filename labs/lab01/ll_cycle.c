c883feac14958a6912db8eb0c15e563ffca5ab98
Somoku <linsh0@protonmail.com>
2022-07-10 11:04:22 +0800

    Lab01 complete.

--- labs/lab01/ll_cycle.c
+++ labs/lab01/ll_cycle.c
@@ -1,7 +1,19 @@
-#include <stddef.h>
 #include "ll_cycle.h"
+#include <stddef.h>
 
 int ll_has_cycle(node *head) {
     /* your code here */
-    return 0;
+    if (!head)
+        return 0;
+    node *hare = head, *tortoise = head;
+    do {
+        for (int i = 0; i < 2; i++) {
+            if (hare->next != NULL)
+                hare = hare->next;
+            else
+                return 0;
+        }
+        tortoise = tortoise->next;
+    } while (hare != tortoise);
+    return 1;
 }
