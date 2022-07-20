e0a282f1c782be44f5800fccc244262b15b02681
Somoku <linsh0@protonmail.com>
2022-07-20 19:58:44 +0800

    Lab02 complete.

--- labs/lab02/lfsr.c
+++ labs/lab02/lfsr.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#include "lfsr.h"
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    *reg = (*reg >> 1) | (((*reg & 0x1) ^ ((*reg >> 2) & 0x1) ^
+                           ((*reg >> 3) & 0x1) ^ ((*reg >> 5) & 0x1))
+                          << 15);
 }
-
--- labs/lab02/vector.c
+++ labs/lab02/vector.c
@@ -1,6 +1,6 @@
 /* Include the system headers we need */
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /* Include our header */
 #include "vector.h"
@@ -58,27 +58,28 @@ vector_t *vector_new() {
     vector_t *retval;
 
     /* First, we need to allocate memory on the heap for the struct */
-    retval = /* YOUR CODE HERE */
+    retval = malloc(sizeof(vector_t)); /* YOUR CODE HERE */
 
     /* Check our return value to make sure we got memory */
-    if (/* YOUR CODE HERE */) {
+    if (retval == NULL /* YOUR CODE HERE */) {
         allocation_failed();
     }
 
     /* Now we need to initialize our data.
        Since retval->data should be able to dynamically grow,
        what do you need to do? */
-    retval->size = /* YOUR CODE HERE */;
-    retval->data = /* YOUR CODE HERE */;
+    retval->size = 1 /* YOUR CODE HERE */;
+    retval->data = malloc(sizeof(int)) /* YOUR CODE HERE */;
 
     /* Check the data attribute of our vector to make sure we got memory */
-    if (/* YOUR CODE HERE */) {
-        free(retval);				//Why is this line necessary?
+    if (retval->data == NULL /* YOUR CODE HERE */) {
+        free(retval); // Why is this line necessary?
         allocation_failed();
     }
 
     /* Complete the initialization by setting the single component to zero */
-    /* YOUR CODE HERE */ = 0;
+    /* YOUR CODE HERE */
+    retval->data[0] = 0;
 
     /* and return... */
     return retval;
@@ -87,8 +88,9 @@ vector_t *vector_new() {
 /* Return the value at the specified location/component "loc" of the vector */
 int vector_get(vector_t *v, size_t loc) {
 
-    /* If we are passed a NULL pointer for our vector, complain about it and exit. */
-    if(v == NULL) {
+    /* If we are passed a NULL pointer for our vector, complain about it and
+     * exit. */
+    if (v == NULL) {
         fprintf(stderr, "vector_get: passed a NULL vector.\n");
         abort();
     }
@@ -96,8 +98,8 @@ int vector_get(vector_t *v, size_t loc) {
     /* If the requested location is higher than we have allocated, return 0.
      * Otherwise, return what is in the passed location.
      */
-    if (loc < /* YOUR CODE HERE */) {
-        return /* YOUR CODE HERE */;
+    if (loc < v->size /* YOUR CODE HERE */) {
+        return v->data[loc] /* YOUR CODE HERE */;
     } else {
         return 0;
     }
@@ -105,8 +107,9 @@ int vector_get(vector_t *v, size_t loc) {
 
 /* Free up the memory allocated for the passed vector.
    Remember, you need to free up ALL the memory that was allocated. */
-void vector_delete(vector_t *v) {
-    /* YOUR SOLUTION HERE */
+void vector_delete(vector_t *v) { /* YOUR SOLUTION HERE */
+    free(v->data);
+    free(v);
 }
 
 /* Set a value in the vector. If the extra memory allocation fails, call
@@ -117,4 +120,26 @@ void vector_set(vector_t *v, size_t loc, int value) {
      */
 
     /* YOUR SOLUTION HERE */
+    if (loc < v->size)
+        v->data[loc] = value;
+    else {
+        vector_t *new_v = malloc(sizeof(vector_t));
+        if (new_v == NULL)
+            allocation_failed();
+        new_v->size = loc + 1;
+        new_v->data = malloc(sizeof(int) * new_v->size);
+        if (new_v->data == NULL) {
+            free(new_v);
+            allocation_failed();
+        }
+        int index = 0;
+        for (; index < v->size; index++)
+            new_v->data[index] = v->data[index];
+        for (; index < new_v->size; index++)
+            new_v->data[index] = 0;
+        new_v->data[loc] = value;
+        free(v->data);
+        *v = *new_v;
+        free(new_v);
+    }
 }
